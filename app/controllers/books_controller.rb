1c2f114c8a584becd4d249c40e388b3bf5beb2ec
Knaokijkl <128217098+Knaokijkl@users.noreply.github.com>
2023-06-05 16:21:22 +0000

    finish bookers2

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,61 @@
+class BooksController < ApplicationController
+  before_action :correct_user, only: [:edit, :update]
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to '/books'
+    else
+      @books = Book.all
+      @user = current_user
+      render :index
+    end
+
+  end
+
+  def show
+    @books = Book.find(params[:id])
+    @user = current_user
+    @book = Book.new
+  end
+
+  def index
+    @books = Book.all
+    @user = current_user
+    @book = Book.new
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(book.id)
+    else
+      redirect_to book_path(book.id)
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body, :user_id, :image)
+  end
+
+  def correct_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    redirect_to(books_path) unless @user == current_user
+  end
+end
